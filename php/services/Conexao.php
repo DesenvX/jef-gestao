d69a8639568602b2335a6aa26a6448809daf3a8a
JoaoPedroSH <joaopedro.shalom27@gmail.com>
2023-01-21 05:32:14 -0300

    update

---
+++ php/services/Conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../../config/Environment.php');
+
+use config\Environment;
+
+Environment::load();
+
+$host = "{$_ENV['HOST']}";
+$user = "{$_ENV['USER']}";
+$pass = "{$_ENV['PASSWORD']}"; //Alterei por causa do meu banco de dados
+$db = "{$_ENV['DATABASE']}";
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+if ($mysqli->connect_errno) {
+    echo "Falha na Conexao: " . $mysqli->connect_error;
+    exit();
+}
