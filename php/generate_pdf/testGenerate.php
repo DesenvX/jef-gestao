99165e7c5805db9d4bb75d943236df2f9ccbd9d0
JoaoPedroSH <joaopedro.shalom27@gmail.com>
2023-01-21 08:26:21 -0300

    update

--- php/generate_pdf/testGenerate.php
+++ php/generate_pdf/testGenerate.php
@@ -1,7 +1,39 @@
 <?php
 
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once 'dompdf/autoload.inc.php';
 
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+
+$header = '
+<table>
+    <tr>
+        <td style="text-align: center;">Cabeçalho do PDF</td>
+    </tr>
+</table>
+';
+
+$body = '
+<h1>Exemplo de uso do Dompdf</h1>
+<p>Este é um exemplo simples de como usar o Dompdf para criar um arquivo PDF com um cabeçalho, corpo e rodapé.</p>
+';
+
+$footer = '
+<table>
+    <tr>
+        <td style="text-align: center;">Rodapé do PDF</td>
+    </tr>
+</table>
+';
+
+$html = $header . $body . $footer;
+
+$dompdf->loadHtml($html);
+
+$dompdf->setPaper('A4', 'portrait');
+
+$dompdf->render();
+
+$dompdf->stream("nome_do_arquivo.pdf", array("Attachment" => false));
+?>
