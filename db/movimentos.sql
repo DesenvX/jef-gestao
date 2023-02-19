ca349a8594b595585ecb673aaa7e593fbddf87e7
Pedro Henrique Pinheiro Silva <pedrohenriquevgtp@gmail.com>
2023-02-19 05:40:33 -0300

    Create movimentos.sql

---
+++ db/movimentos.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 19-Fev-2023 às 08:40
+-- Versão do servidor: 8.0.17
+-- versão do PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `jef_gestao`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `movimentos`
+--
+
+CREATE TABLE `movimentos` (
+  `id` int(11) NOT NULL,
+  `hora_inicial` varchar(255) NOT NULL,
+  `hora_final` varchar(255) NOT NULL,
+  `horas_trabalhadas` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `dia_semana` varchar(500) NOT NULL,
+  `valor_diaria` varchar(255) NOT NULL,
+  `id_colaborador` int(11) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `id_maquina` int(11) NOT NULL,
+  `id_pasto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `movimentos`
+--
+ALTER TABLE `movimentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `movimentos`
+--
+ALTER TABLE `movimentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
